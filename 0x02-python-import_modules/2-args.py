2b3bc73e2ee3502bede0949633fc2da7293e0477
zeezatcoder <azeezatmaja@gmail.com>
2022-08-28 10:58:53 -0700

    Bug

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
         for i in range(1, x):
             print("{:d}: {}".format(x - 1, sys.argv[i]))
     else:
-         print("{:d} {}:".format(x - 1, "arguments"))
-         for i in range(1, x):
-             print("{:d}: {}".format(i, sys.argv[i]))
+        print("{:d} {}:".format(x - 1, "arguments"))
+        for i in range(1, x):
+            print("{:d}: {}".format(i, sys.argv[i]))
