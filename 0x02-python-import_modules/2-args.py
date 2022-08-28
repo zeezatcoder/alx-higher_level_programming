5e40fe9c79f3eba789d863db72632f0b705a95e6
zeezatcoder <azeezatmaja@gmail.com>
2022-08-28 10:24:43 -0700

    module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         print("{:d} {}:".format(1, "argument"))
         for i in range(1, x):
             print("{:d}: {}".format(x - 1, sys.argv[i]))
-        else:
-            print("{:d} {}:".format(x - 1, "arguments"))
-            for i in range(1, x):
-                print("{:d}: {}".format(i, sys.argv[i]))
+    else:
+         print("{:d} {}:".format(x - 1, "arguments"))
+         for i in range(1, x):
+             print("{:d}: {}".format(i, sys.argv[i]))
