d567b137359c547e37a3ddf4eb88965ae3034258
zeezatcoder <azeezatmaja@gmail.com>
2022-09-01 04:20:33 -0700

    pushing

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,4 +6,4 @@ def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
-        return (new_matrix)
+    return (new_matrix)
