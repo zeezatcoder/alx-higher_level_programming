52242c6bbf1601d4d824abff707651ff351f0c8e
zeezat <azeezatmaja@gmail.com>
2022-09-14 03:04:38 -0700

    fix5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """ defines a square class """
+
+    def __init__(self, size=0) -> None:
+        """
+        Intialize the attributes
+        Args:
+            size: size of square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """ defines a getter for size attributes """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ computes the area of a square """
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            count = 0
+            while count < self.__size:
+                num = 0
+                while num < self.__size:
+                    print("{}".format("#"), end='')
+                    num += 1
+            print()
+            count += 1
