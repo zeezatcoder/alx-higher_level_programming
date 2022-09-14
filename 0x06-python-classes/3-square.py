afa199902d8942032c75f4468cf29c211bf1707c
zeezat <azeezatmaja@gmail.com>
2022-09-14 01:38:29 -0700

    3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Square module """
+
+class Square:
+    """ defines a square class """
+
+    def __init__(self, size=0) -> None:
+        """
+        Intializes class attributes
+        Args:
+        size: size of square
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+            def area(self):
+                """
+                calculate the area of a square
+                """
+                return self.__size ** 2
