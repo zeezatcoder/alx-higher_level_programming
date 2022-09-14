96d82047754db8555b0d33af6b1a5d709173b790
zeezat <azeezatmaja@gmail.com>
2022-09-14 01:28:14 -0700

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """ defines a square class """
+
+    def __init__(self, size) -> None:
+        """
+        Initialize class attributes
+        Args:
+        size: size of square
+        """
+        self.__size = size
